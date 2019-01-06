30fb010e626061b8ef4b743ba0dad64f27a43600
dongtranthien <thiendong.iuh@gmail.com>
2019-01-07 00:09:16 +0700

    Fix bug over index array and add feature update pin status

--- sketch_jan06c/sketch_jan06c.ino
+++ sketch_jan06c/sketch_jan06c.ino
@@ -14,31 +14,32 @@ enum node_type {
 
 //Modified
 //ID and Type Node
-const unsigned char ID_NODE = 27;
-const unsigned char NODE = RLACS;
+#define ID_NODE             27
+#define NODE                RLTDS
+#define DEBUG
+
 //Hardware of RLACS Node
-#define PIN_D1    3
-#define PIN_D2    4
-#define PIN_A1    A0
-#define PIN_A2    A1
+#define PIN_D1              3
+#define PIN_D2              4
+#define PIN_A1              A0
+#define PIN_A2              A1
 //Hardware of RLTDS Node
-#define PIN_D     5
-#define PIN_TDS   A2
+#define PIN_D               5
+#define PIN_TDS             A2
 //Hardware of THL Node
-#define PIN_T     A3
-#define PIN_H     A4
-#define PIN_L     A5
+#define PIN_T               A3
+#define PIN_H               A4
+#define PIN_L               A5
 
 #define ID_NODE_LENGTH      2
-//#define DEBUG
-char rlacs_node[5] = {'R','L','A','C','S'};
-char rltds_node[5] = {'R','L','T','D','S'};
-char thl_node[3] = {'T','H','L'};
+
+//Length and string of type node
 char node_name_str[3][5] = {{'R','L','A','C','S'},
                             {'R','L','T','D','S'},
                             {'T','H','L',' ',' '}};
 unsigned char node_name_str_length[3] = {5,5,3};
 
+//Length and String of command
 #define TESTCONNECT_LEN     11
 char command_testConnect[TESTCONNECT_LEN] = {'T','E','S','T','C','O','N','N','E','C','T'};
 #define GETDATA_LEN         7
@@ -70,9 +71,14 @@ void CheckConnect(){
     Serial.println("CHECK_CONNECT");
   #endif
 
+  int index_temp;
   //Check String "TestConnect" command of string receive
   for(j = TESTCONNECT_LEN; j > 0; j--){        
-    if(rec[index_rec - TESTCONNECT_LEN + j - 1] != command_testConnect[j - 1]){ //Compare
+    index_temp = index_rec - TESTCONNECT_LEN + j - 1;
+    if(index_temp < 0){
+      index_temp = index_temp + 200;
+    }
+    if(rec[index_temp] != command_testConnect[j - 1]){ //Compare
       #ifdef DEBUG 
         Serial.println("CHECK_CONNECT-ERROR-Command Incorect");
       #endif
@@ -82,21 +88,15 @@ void CheckConnect(){
   
   //Check Name of Node on string receive, if different then return
   for(j = node_name_str_length[NODE]; j > 0; j--){    
-    if((index_rec - len + j - 1) >= 0){                                 //Check index over max array (rec)      
-      if(rec[index_rec - len + j - 1] != node_name_str[NODE][j - 1]){   //Check character of string receive vs node name string
-        #ifdef DEBUG 
-          Serial.println("CHECK_CONNECT-ERROR-Node Name Incorect");
-        #endif
-        return;
-      }
+    index_temp = index_rec - len + j - 1;
+    if(index_temp < 0){
+      index_temp = index_temp + 200;
     }
-    else{
-      if(rec[200 - index_rec + len - j - 1] != node_name_str[NODE][j - 1]){
-        #ifdef DEBUG 
-          Serial.println("CHECK_CONNECT-ERROR-Node Name Incorect");
-        #endif
-        return;
-      }
+    if(rec[index_temp] != node_name_str[NODE][j - 1]){   //Check character of string receive vs node name string
+      #ifdef DEBUG 
+        Serial.println("CHECK_CONNECT-ERROR-Node Name Incorect");
+      #endif
+      return;
     } 
   }
 
@@ -104,7 +104,11 @@ void CheckConnect(){
   int node_index_temp = 0;    //init id node index data  
   //Calculate id Node number from string
   for(j = 0; j < ID_NODE_LENGTH > 0; j++){
-    node_index_temp = node_index_temp*10 + rec[index_rec - len + node_name_str_length[NODE] + j] - 48;    
+    index_temp = index_rec - len + node_name_str_length[NODE] + j;
+    if(index_temp < 0){
+      index_temp = index_temp + 200;
+    }
+    node_index_temp = node_index_temp*10 + rec[index_temp] - 48;    
   }    
   //Compare ID Number from String receive vs ID Node(Setup)
   if(node_index_temp != ID_NODE){
@@ -208,8 +212,13 @@ void GetData(){
   #endif
 
   //Check String "GetData" command of string receive
+  int index_temp;
   for(j = GETDATA_LEN; j > 0; j--){    
-    if(rec[index_rec - GETDATA_LEN + j - 1] != command_getData[j - 1]){ //Compare
+    index_temp = index_rec - GETDATA_LEN + j - 1;
+    if(index_temp < 0){
+      index_temp = index_temp + 200;
+    }
+    if(rec[index_temp] != command_getData[j - 1]){ //Compare
       #ifdef DEBUG 
         Serial.println("Error-GetData-Command Incorect");
       #endif
@@ -219,29 +228,27 @@ void GetData(){
   
   //Check Name of Node on string receive, if different then return
   for(j = node_name_str_length[NODE]; j > 0; j--){    
-    if((index_rec - len + j - 1) >= 0){                                 //Check index over max array (rec)      
-      if(rec[index_rec - len + j - 1] != node_name_str[NODE][j - 1]){   //Check character of string receive vs node name string
-        #ifdef DEBUG 
-          Serial.println("Error-GetData-Node Name Incorect");
-        #endif
-        return;
-      }
+    index_temp = index_rec - len + j - 1;
+    if(index_temp < 0){
+      index_temp = index_temp + 200;
     }
-    else{
-      if(rec[200 - index_rec + len - j - 1 - 2] != node_name_str[NODE][j - 1]){
-        #ifdef DEBUG 
-          Serial.println("Error-GetData-Node Name Incorect");
-        #endif
-        return;
-      }
-    } 
+    if(rec[index_temp] != node_name_str[NODE][j - 1]){   //Check character of string receive vs node name string
+      #ifdef DEBUG 
+        Serial.println("Error-GetData-Node Name Incorect");
+      #endif
+      return;
+    }   
   }
 
   //Check ID Node Number of node
   int node_index_temp = 0;    //init id node index data  
   //Calculate id Node number from string
   for(j = 0; j < ID_NODE_LENGTH > 0; j++){
-    node_index_temp = node_index_temp*10 + rec[index_rec - len + node_name_str_length[NODE] + j] - 48;    
+    index_temp = index_rec - len + node_name_str_length[NODE] + j;
+    if(index_temp < 0){
+      index_temp = index_temp + 200;
+    }
+    node_index_temp = node_index_temp*10 + rec[index_temp] - 48;    
   }  
   
   //Compare ID Number from String receive vs ID Node(Setup)
@@ -338,9 +345,10 @@ void GetData(){
       Serial.print("_");
       Serial.print(analogRead(PIN_L)); 
       break;
-    }
-    Serial.println("");                   //End of transfer
+    }    
   }
+  Serial.println("");                   //End of transfer
+
   #ifdef DEBUG
     Serial.println("GETDATA OK - End Function");
   #endif
@@ -355,12 +363,25 @@ void UpdateData(){
   #endif
 
   //Get Status Data Update to calculate length of status from string receive
-  if((rec[index_rec - 2] == 'O')&&(rec[index_rec - 1] == 'N')){
+  int index_temp1, index_temp2, index_temp3;
+  index_temp1 = index_rec - 1;
+  index_temp2 = index_rec - 2;
+  index_temp3 = index_rec - 3;
+  if(index_temp1 < 0){
+    index_temp1 = 200 + index_temp1;
+  }
+  if(index_temp2 < 0){
+    index_temp2 = 200 + index_temp2;
+  }
+  if(index_temp3 < 0){
+    index_temp3 = 200 + index_temp3;
+  }
+  if((rec[index_temp2] == 'O')&&(rec[index_temp1] == 'N')){
     //Calculate Length of String Receive
     len_status_str = 2;     //Length of 'ON' string     
   }
   else{
-    if((rec[index_rec - 3] == 'O')&&(rec[index_rec - 2] == 'F')&&(rec[index_rec - 1] == 'F')){
+    if((rec[index_temp3] == 'O')&&(rec[index_temp2] == 'F')&&(rec[index_temp1] == 'F')){
       //Calculate Length of String Receive
       len_status_str = 3;     //Length of status 'OFF' string        
     }
@@ -373,11 +394,20 @@ void UpdateData(){
   }
 
   //Get length of pin str on string receive
-  if(rec[index_rec - 4 - len_status_str] == '_'){
+  index_temp1 = index_rec - 4 - len_status_str;
+  index_temp2 = index_rec - 3 - len_status_str;
+  if(index_temp1 < 0){                            //Calib index data if over array index
+    index_temp1 = index_temp1 + 200;
+  }
+  if(index_temp2 < 0){                            //Calib index data if over array index
+    index_temp2 = index_temp2 + 200;
+  }
+
+  if(rec[index_temp1] == '_'){
     len_pin_str = 2;
   }
   else{
-    if(rec[index_rec - 3 - len_status_str] == '_'){
+    if(rec[index_temp2] == '_'){
       len_pin_str = 1;
     }
     else{
@@ -386,6 +416,18 @@ void UpdateData(){
       #endif
       return;
     }
+  }
+
+  //Check fomated of pin
+  index_temp1 = index_rec - len_status_str - 1 - len_pin_str;
+  if(index_temp1 < 0){                            //Calib index data if over array index
+    index_temp1 = index_temp1 + 200;
+  }
+  if(rec[index_temp1] != 'D'){
+    #ifdef DEBUG 
+      Serial.println("UPDATEDATA-ERROR-PIN unformatted");
+    #endif
+    return;
   }
 
   //Calculate length of string receive from uart
@@ -425,8 +467,12 @@ void UpdateData(){
   }*/
 
   //Check String "UpdateData" command of string receive
-  for(j = UPDATEDATA_LEN; j > 0; j--){        
-    if(rec[index_rec - UPDATEDATA_LEN + j - 1 - len_pin_str - len_status_str - 2] != command_updatedata[j - 1]){ //Compare     //2 space '_' in str receive
+  for(j = UPDATEDATA_LEN; j > 0; j--){ 
+    index_temp1 = index_rec - UPDATEDATA_LEN + j - 1 - len_pin_str - len_status_str - 2;
+    if(index_temp1 < 0){                          //Calib index data if over array index
+      index_temp1 = index_temp1 + 200;
+    }     
+    if(rec[index_temp1] != command_updatedata[j - 1]){ //Compare     //2 space '_' in str receive
       #ifdef DEBUG 
         Serial.println("UPDATEDATA-ERROR-Command Incorect");
       #endif
@@ -435,22 +481,16 @@ void UpdateData(){
   }
 
   //Check Name of Node on string receive, if different then return
-  for(j = node_name_str_length[NODE]; j > 0; j--){  
-    if((index_rec - len + j - 1) >= 0){                                 //Check index over max array (rec)      
-      if(rec[index_rec - len + j - 1] != node_name_str[NODE][j - 1]){   //Check character of string receive vs node name string
-        #ifdef DEBUG 
-          Serial.println("UPDATEDATA-ERROR-Node Name Incorect");
-        #endif
-        return;
-      }
-    }
-    else{
-      if(rec[200 - index_rec + len - j - 1 - 2] != node_name_str[NODE][j - 1]){
-        #ifdef DEBUG 
-          Serial.println("UPDATEDATA-ERROR-Node Name Incorect");
-        #endif
-        return;
-      }
+  for(j = node_name_str_length[NODE]; j > 0; j--){
+    index_temp1 = index_rec - len + j - 1;
+    if(index_temp1 < 0){                          //Calib index data if over array index
+      index_temp1 = index_temp1 + 200;
+    } 
+    if(rec[index_temp1] != node_name_str[NODE][j - 1]){   //Check character of string receive vs node name string
+      #ifdef DEBUG 
+        Serial.println("UPDATEDATA-ERROR-Node Name Incorect");
+      #endif
+      return;
     }
   }
 
@@ -458,7 +498,11 @@ void UpdateData(){
   int node_index_temp = 0;    //init id node index data  
   //Calculate id Node number from string
   for(j = 0; j < ID_NODE_LENGTH > 0; j++){
-    node_index_temp = node_index_temp*10 + rec[index_rec - len + node_name_str_length[NODE] + j] - 48;    
+    index_temp1 = index_rec - len + node_name_str_length[NODE] + j;
+    if(index_temp1 < 0){
+      index_temp1 = index_temp1 + 200;
+    }
+    node_index_temp = node_index_temp*10 + rec[index_temp1] - 48;    
   }  
   
   //Compare ID Number from String receive vs ID Node(Setup)
@@ -469,7 +513,70 @@ void UpdateData(){
     return;
   }
 
-  //OK, UpdateData Command correct. Now get data and Transfer data to sever  
+  //OK, UpdateData Command correct. Now update data and Transfer data to sever  
+  //Update Data
+  switch(NODE){
+    case RLACS:{
+      if(len_pin_str != 2){
+        #ifdef DEBUG
+          Serial.println("UPDATEDATA-ERROR-Pin in node unformatted");
+        #endif
+        return;
+      }
+      index_temp1 = index_rec - len_status_str - 1 - 1;
+      if(index_temp1 < 0){
+        index_temp1 = index_temp1 + 200;
+      }
+      if(rec[index_temp1] == 1){
+        if(len_status_str == 3){
+          digitalWrite(PIN_D1, LOW);          
+        }
+        else{
+          digitalWrite(PIN_D1, HIGH);
+        }
+      }
+      else{
+        if(rec[index_temp1] == 2){
+          if(len_status_str == 3){
+            digitalWrite(PIN_D2, LOW);          
+          }
+          else{
+            digitalWrite(PIN_D2, HIGH);
+          }
+        }
+        else{
+          #ifdef DEBUG
+            Serial.println("UPDATEDATA-ERROR-Pin in node unformatted");
+          #endif
+          return;
+        }
+      }
+      break;
+    }
+    case RLTDS:{
+      if(len_pin_str != 1){
+        #ifdef DEBUG
+          Serial.println("UPDATEDATA-ERROR-Pin in node unformatted");
+        #endif
+        if(len_status_str == 3){
+          digitalWrite(PIN_D, LOW);          
+        }
+        else{
+          digitalWrite(PIN_D, HIGH);
+        }
+        return;
+      }
+
+      break;
+    }
+    case THL:{
+      #ifdef DEBUG
+        Serial.println("UPDATEDATA-ERROR-THL Node not update Data");
+      #endif
+      return;
+      break;
+    }
+  }
   //Transfer String response 
   /*Format:
       NODEID_UPDATE_PIN_STATUS
@@ -485,9 +592,17 @@ void UpdateData(){
   Serial.print("_");
   Serial.print("UPDATE");                       //Transfer Data string
   Serial.print("_");
-  Serial.print(rec[index_rec - len_status_str - 1 - len_pin_str]);    //1 space '_' on string         
+  index_temp1 = index_rec - len_status_str - 1 - len_pin_str;
+  if(index_temp1 < 0){
+    index_temp1 = index_temp1 + 200;
+  }
+  Serial.print(rec[index_temp1]);    //1 space '_' on string         
   if(len_pin_str == 2){     //If pin = D2,D1 and not if pin = D
-    Serial.print(rec[index_rec - len_status_str - 1 - len_pin_str + 1]);    //1 space '_' on string     //Transfer digit of pin
+    index_temp1 = index_rec - len_status_str - 1 - len_pin_str + 1;
+    if(index_temp1 >= 200){
+      index_temp1 = index_temp1 - 200;
+    }
+    Serial.print(rec[index_temp1]);    //1 space '_' on string     //Transfer digit of pin
   }
   Serial.print("_");
   Serial.print("OK");
